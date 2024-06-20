3b540e9cc2cd1554030a9b0e9d96d172030326d1
Kk4ssio <diasmonteirokassio@gmail.com>
2024-06-20 17:05:58 -0300

    v6

--- Game.js
+++ Game.js
@@ -5,8 +5,11 @@ function Acessar() {
     var nome = document.getElementById("nome").value.trim();
 
     if (nome === "") {
+
         alert("Insira seu nome para ter acesso ao jogo!");
-    } else {
+
+    }else{
+
         setTimeout(function () {
             window.location.href = "Jogo.html";
         }, 3000);
@@ -15,7 +18,8 @@ function Acessar() {
 
 }
 
-var advinhacao = Math.floor(Math.random() * 100);
+var advinhacao = Math.floor(Math.random() * 100) +1;
+
 function adivinhar() {
     var resultado = document.getElementById("resultado");
     var valorinserido = parseInt(document.getElementById("numero").value);
@@ -23,43 +27,56 @@ function adivinhar() {
     console.log(advinhacao);
 
     if (valorinserido == advinhacao) {
+
         resultado.textContent = "PARABÉNS! O número que você adivinhou é: " + advinhacao + ". Tentativas: " + Tentativas;
         resultado.style.color = 'green';
         pontuacao += 10;
 
         setTimeout(function () {
+
             window.location.href = "Fim.html";
         }, 3000);
 
-    } else {
-
+    }else{
         Tentativas += 1;
         if (distancia < 10) {
-            resultado.style.color = 'red'; // Muito quente
-        } else if (distancia < 20) {
+            resultado.style.color = 'red'; // Muito quentE
+
+        }
+
+        else if (distancia < 20) {
             resultado.style.color = 'orange'; // Quente
-        } else if (distancia < 30) {
+
+        }else if (distancia < 30){
             resultado.style.color = 'yellow'; // Morno
-        } else {
+
+        }else{
             resultado.style.color = 'blue'; // Frio
+
         }
 
         if (valorinserido > advinhacao) {
+
             resultado.textContent = "NÃO FOI DESTA VEZ, AMIGO. Tente um número menor! Tentativas: " + Tentativas;
-        } else {
+
+        }else{
             resultado.textContent = "NÃO FOI DESTA VEZ, AMIGO. Tente um número maior! Tentativas: " + Tentativas;
+
         }
+
     }
 }
 
 function exibirPontuacao() {
     alert("Sua pontuação atual é: " + pontuacao);
+
 }
 
 function denovo() {
+    window.location.href = "Jogo.html";
 
-        window.location.href = "Jogo.html";
 }
+
 function novoPlayer() {
     window.location.href = "Cadastro.html";
 
